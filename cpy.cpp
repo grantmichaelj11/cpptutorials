9e59ac6c8283de053cf9c1ad23ab242de5a44ba3
grantmichaelj11 <grantmichaelj11@gmail.com>
2025-02-06 23:36:36 -0500

    Added a method to randomize each element

--- cpy.cpp
+++ cpy.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <random>
 using namespace std;
 
 
@@ -15,11 +16,31 @@ class Matrix{
 			z = zi;
 			matrix = vector<vector<vector<double>>>(xi, vector<vector<double>>(yi, vector<double>(zi, 0.0)));
 		}
+
+	void randomizeMatrixUniform(float low, float high){
+		// Takes random numbers between low and high
+
+		// seeds random number generator
+		random_device rd;
+		//define our random number generator
+		mt19937 mt(rd());
+		//Generate the random number on some range
+		uniform_real_distribution<double> dist(low, high);
+
+		//Iterate through matrix and randomly assign values to each element
+		for (int i = 0; i < matrix.size(); i++){
+			for (int j = 0; j < matrix[i].size(); j++){
+				for (int k = 0; k < matrix[i][j].size(); k++){
+					matrix[i][j][k] = dist(mt);
+				}
+			}
+		}
+	}
 };
 
 int main(){
 	Matrix testMat(3,3,3);
-
+	testMat.randomizeMatrixUniform(-0.5, 0.5);
 	for (int i = 0; i < testMat.matrix.size(); ++i){
 		for (int j = 0; j < testMat.matrix[i].size(); ++j){
 			for (int k = 0; k < testMat.matrix[i][j].size(); ++k){
