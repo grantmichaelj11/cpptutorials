da15c543a9399f117167810ba9db681a7162509a
grantmichaelj11 <grantmichaelj11@gmail.com>
2025-02-08 22:12:37 -0500

    Created 2D-Matrix class

--- cpy.cpp
+++ cpy.cpp
@@ -14,9 +14,9 @@ class Matrix3D{
 		Matrix3D(int xi, int yi, int zi) {
 			try {
 
-				if (xi == 0 || yi == 0 || zi == 0) {
+				if (xi <= 0 || yi <= 0 || zi <= 0) {
 
-					throw invalid_argument("All three dimensions must be non-zero");
+					throw invalid_argument("All three dimensions must be greater than 0.");
 
 				} else {
 					x = xi;
@@ -63,26 +63,74 @@ class Matrix3D{
 			}
 		}
 	}
+
+	//Print Matrix Function?
+
 };
 
 // Create 2D Matrix Function
+class Matrix2D {
+	public:
+		int x, y;
+		vector<vector<double>> matrix;
+		Matrix2D(int xi, int yi) {
+			try {
 
-// Do we need to create a Vector Function?
+				if (xi <= 0 || yi <= 0) {
+
+					throw invalid_argument("Both dimensions must be greater than 0.");
+
+				} else {
+					x = xi;
+					y = yi;
+
+					//Generate the 2D matrix if all three dimensions exist
+					matrix = vector<vector<double>>(xi, vector<double>(yi, 0.0));
+				}
+			} catch (const invalid_argument& e) {
+				cerr << e.what() << endl;
+			}
+		}
+	void randomizeMatrixUniform(float low, float high){
+		// Takes random numbers between low and high
+
+		// seeds random number generator
+		random_device rd;
+		mt19937 mt(rd());
+		//define our random number generator
+		uniform_real_distribution<double> dist(low, high);
+
+		//Iterate through matrix and randomly assign values to each element
+		for (int i = 0; i < matrix.size(); i++){
+			for (int j = 0; j < matrix[i].size(); j++){
+				matrix[i][j] = dist(mt);
+			}
+		}
+	}
+
+	void randomizeMatrixGaussian(float mean, float std){
+		//Randomly initalizes matrix according to a Gaussian Distribution
+		random_device rd;
+		mt19937 mt(rd());
+		normal_distribution<> dist(mean, std);
+		for (int i = 0; i < matrix.size(); i++){
+			for (int j = 0; j < matrix[i].size(); j++){
+				matrix[i][j] = dist(mt);
+			}
+		}
+	}
+};
 
 //Matrix Operations functions?
 
 int main(){
-	Matrix3D testMat(3,3,3);
+	Matrix2D testMat(3,3);
 	testMat.randomizeMatrixGaussian(0.0, 1.0);
 	for (int i = 0; i < testMat.matrix.size(); ++i){
 		for (int j = 0; j < testMat.matrix[i].size(); ++j){
-			for (int k = 0; k < testMat.matrix[i][j].size(); ++k){
-				cout << testMat.matrix[i][j][k] << " ";
-			}
-		cout << endl;
-		}
-	cout << endl;
-	}
+			cout << testMat.matrix[i][j] << " ";
+		} cout << endl;
+	} cout << endl;
 	return 0;
 }
 
